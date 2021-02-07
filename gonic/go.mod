97785bd9db8912b53db528b2d130bdebd9105bf5
yeqown <yeqown@gmail.com>
2021-02-07 21:33:34 +0800

    feat: gormcs v2

--- gonic/go.mod
+++ gonic/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/golang/protobuf v1.4.2
 	github.com/jinzhu/gorm v1.9.8
 	github.com/mxk/go-sqlite v0.0.0-20140611214908-167da9432e1f
+	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.6.0
 	github.com/qmuntal/stateless v1.1.5
@@ -23,7 +24,6 @@ require (
 	github.com/speps/go-hashids v2.0.0+incompatible
 	github.com/stretchr/testify v1.4.0
 	github.com/yeqown/infrastructure v0.3.3
-	go.etcd.io/etcd v0.0.0-20181022230727-86b933311d23
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
@@ -34,7 +34,6 @@ require (
 	gopkg.in/src-d/go-billy.v4 v4.3.0 // indirect
 	gopkg.in/src-d/go-git-fixtures.v3 v3.5.0 // indirect
 	gopkg.in/src-d/go-git.v4 v4.11.0
-	gorm.io/driver/mysql v1.0.4
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.20.12
 )
--- gonic/gormcs/v2/plugin_test.go
+++ gonic/gormcs/v2/plugin_test.go
@@ -1,25 +1,79 @@
 package v2
 
-import gorm2 "gorm.io/gorm"
+import (
+	"context"
+
+	"github.com/playground/gonic/gormcs"
+
+	"github.com/opentracing/opentracing-go"
+	gorm2 "gorm.io/gorm"
+)
 
 // https://gorm.io/zh_CN/docs/prometheus.html
 // https://github.com/go-gorm/prometheus
 
 type opentracingPlugin struct{}
 
+func (o opentracingPlugin) AfterCreate(db *gorm2.DB) {
+	sp := db.Statement.Context.Value(gormContextSpan).(opentracing.Span)
+	sp.Finish()
+
+	println("called after")
+
+	return
+}
+
+func (o opentracingPlugin) BeforeCreate(db *gorm2.DB) {
+	sp := opentracing.SpanFromContext(db.Statement.Context)
+	if sp != nil {
+		sp = opentracing.StartSpan(
+			"gorm2/create",
+			opentracing.ChildOf(sp.Context()),
+		)
+	} else {
+		sp = opentracing.StartSpan("gorm2/create")
+	}
+
+	println("called before")
+	db.Statement.Context = context.WithValue(db.Statement.Context, gormContextSpan, sp)
+
+	return
+}
+
 func (o opentracingPlugin) Name() string {
 	return "opentracing"
 }
 
 func (o opentracingPlugin) Initialize(db *gorm2.DB) error {
-	panic("implement me")
+	println("opentracing initialized")
+
+	// register callbacks
+	_ = db.Callback().Create().Before("gorm:create").Register("opentracing:before_create", o.BeforeCreate)
+	_ = db.Callback().Create().After("gorm:create").Register("opentracing:after_create", o.AfterCreate)
+
+	// set tracer
+	return nil
 }
+
+var gormContextSpan struct{}
 
 func newPlugin() gorm2.Plugin {
 	return opentracingPlugin{}
 }
 
 func (g gorm2TestSuite) Test_withPlugin() {
-	err := g._db.Use(newPlugin())
+	err := g.db().Use(newPlugin())
 	g.Require().Nil(err)
+	g.Require().NotNil(g.db())
+
+	loc := &gormcs.LocationModel{
+		UserID:   1,
+		Country:  "CN",
+		Province: "Sichuan",
+		City:     "Chengdu",
+	}
+
+	// create
+	err = g.db().WithContext(context.Background()).Create(loc).Error
+	g.Assert().Nil(err)
 }
