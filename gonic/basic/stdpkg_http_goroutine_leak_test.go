611269915a3bf34828d2c54b190272cf26ea6465
yeqown <yeqown@gmail.com>
2021-04-14 13:19:18 +0800

    feat: new unittest

---
+++ gonic/basic/stdpkg_http_goroutine_leak_test.go
@@ -0,0 +1,34 @@
+package basic
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+func Test_Client(t *testing.T) {
+	num := 20
+
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+	for index := 0; index < num; index++ {
+		go func() {
+			defer wg.Done()
+			resp, _ := http.Get("https://www.baidu.com")
+			_, _ = ioutil.ReadAll(resp.Body)
+		}()
+	}
+
+	println("break point for debug")
+	wg.Wait()
+
+	fmt.Printf("此时goroutine个数= %d\n", runtime.NumGoroutine())
+	println("break point for debug")
+}
+
+func Test_Server(t *testing.T) {
+
+}
