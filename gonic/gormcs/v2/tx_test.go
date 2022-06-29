5ef194a4f0fd7ee179c952f5fd06e6479f995718
yeqown <yeqown@gmail.com>
2022-06-29 09:31:14 +0800

    chore: gonic update

---
+++ gonic/gormcs/v2/tx_test.go
@@ -0,0 +1,32 @@
+package v2
+
+import (
+	"sync"
+	"time"
+
+	"github.com/playground/gonic/gormcs"
+)
+
+func (g *gorm2TestSuite) Test_bench_tx() {
+	wg := sync.WaitGroup{}
+	routine := func() {
+		defer wg.Done()
+		tx := g.db().Model(&gormcs.UserModel{})
+		tx.Begin()
+
+		out := new(gormcs.UserModel)
+		if err := tx.Where("id = ?", 1).First(out).Error; err != nil {
+			return
+		}
+
+		time.Sleep(time.Millisecond * 10)
+	}
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go routine()
+	}
+
+	wg.Wait()
+	time.Sleep(10 * time.Second)
+}
