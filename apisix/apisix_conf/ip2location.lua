4e275fab1fc8c11be15e9d91f5bea1457cb37379
yeoqwn <yeqown@gmail.com>
2024-09-14 15:51:08 +0800

    feat: update

--- apisix/apisix_conf/ip2location.lua
+++ apisix/apisix_conf/ip2location.lua
@@ -18,9 +18,32 @@ function _M.check_schema(conf)
 end
 
 function _M.rewrite(conf, ctx)
-    local header_a = core.request.header(ctx, "X-Real-IP")
-    if header_a then
-        core.request.set_header(ctx, "X-Real-IP2", header_a)
+    -- "cloudfront-viewer-city": "Hong Kong" 设置到 X-City
+    -- "cloudfront-viewer-country": "SG" 设置到 X-Country-Short Country-Short
+    -- "cloudfront-viewer-country-name": "Singapore" 设置到 X-Country-Long
+    -- "cloudfront-viewer-region-name": "Singapore" 设置到 X-Region
+
+    local headers = ctx.var.headers
+    local city = headers["cloudfront-viewer-city"]
+    local country = headers["cloudfront-viewer-country"]
+    local country_name = headers["cloudfront-viewer-country-name"]
+    local region = headers["cloudfront-viewer-region-name"]
+
+    if city then
+        core.request.set_header("X-City", city)
+    end
+
+    if country then
+        core.request.set_header("X-Country-Short", country)
+        core.request.set_header("Country-Short", country)
+    end
+
+    if country_name then
+        core.request.set_header("X-Country-Long", country_name)
+    end
+
+    if region then
+        core.request.set_header("X-Region", region)
     end
 end
 
