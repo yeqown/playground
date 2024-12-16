3ba0d09cd422dfc635b6825e718b530e69185507
yeoqwn <yeqown@gmail.com>
2024-12-16 17:32:48 +0800

    feat: tampermonkey/jenkins-hack updated

--- javascript/tampermonkey/jenkins-hack.js
+++ javascript/tampermonkey/jenkins-hack.js
@@ -12,6 +12,7 @@
 
 // 当前页面加载完成后，从中定位到 id="ecp_DeployMicroServices" 的元素
 // 在前面插入两个按钮，一个是全选（id="selectAllButton"），一个是取消全选（id="deselectAllButton"）
+// 再新增一个元素选中数量的说明：id="selectedCount"，内容为当前选中的数量 / 总数量
 // 全选的作用是，将下面这个列表中的所有的 checkbox 都选中
 // 取消全选的作用是，将下面这个列表中的所有的 checkbox 都取消选中
 // <table id="tbl_ecp_DeployMicroServices">
@@ -37,10 +38,19 @@
         console.warn('table(tbl_ecp_DeployMicroServices) not found');
         return;
     }
+    const tableParent = document.getElementById('ecp_DeployMicroServices');
+    if (!tableParent) {
+        console.warn('table parent(ecp_DeployMicroServices) not found');
+        return;
+    }
+    tableParent.style = 'float: left; padding-right: 25px;';
 
     const selectAllButton = document.createElement('button');
     selectAllButton.innerText = '全选';
     selectAllButton.id = 'selectAllButton';
+    // selectAllButton.classList.add('jenkins-button');
+    // selectAllButton.classList.add('jenkins-button--secondary');
+    // selectAllButton.style.paddingRight = '1em';
     selectAllButton.onclick = function (evt) {
         evt.preventDefault();
         const checkboxes = table.querySelectorAll('input[type="checkbox"]');
@@ -52,6 +62,9 @@
     const deselectAllButton = document.createElement('button');
     deselectAllButton.innerText = '取消全选';
     deselectAllButton.id = 'deselectAllButton';
+    // deselectAllButton.classList.add('jenkins-button');
+    // deselectAllButton.classList.add('jenkins-button--secondary');
+    // deselectAllButton.style.paddingRight = '1em';
     deselectAllButton.onclick = function (evt) {
         evt.preventDefault();
         const checkboxes = table.querySelectorAll('input[type="checkbox"]');
@@ -60,6 +73,16 @@
         });
     };
 
+    const selectedCount = document.createElement('span');
+    selectedCount.id = 'selectedCount';
+    selectedCount.innerText = '0 / 0';
+    table.addEventListener('change', function (evt) {
+        const checkboxes = table.querySelectorAll('input[type="checkbox"]');
+        const selected = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+        selectedCount.innerText = `${selected.length} / ${checkboxes.length}`;
+    });
+
     table.parentNode.insertBefore(selectAllButton, table);
     table.parentNode.insertBefore(deselectAllButton, table);
+    table.parentNode.insertBefore(selectedCount, table);
 })();
